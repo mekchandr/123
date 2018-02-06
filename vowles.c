c5af9971ffc95273ea8360913b9f677bf3356f1d
mekchandr <35653274+mekchandr@users.noreply.github.com>
2018-02-06 16:06:11 +0530

    Create vowles.c

---
+++ vowles.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char a;
+    scanf("%c",&a);
+    if (a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+    printf("is vowles",a);
+    else
+    printf("is consonent",a);
+}
